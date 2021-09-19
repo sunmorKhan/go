8ba7b565a4d613b699f04287ef0f4811a0a4f11b
sunmorKhan <tjdah109@naver.com>
2021-09-19 20:13:43 +0900

    ver02

--- syntax04.go
+++ syntax04.go
@@ -1,4 +1,4 @@
-package main //ver0.1
+package main //ver0.2
 
 import (
 	"fmt"
@@ -14,12 +14,12 @@ func main()  { // 2~150까지 소수 판별기 소수 : 1과 자기자신 이외
 	number := rand.Intn(149) + 2 // 2 더함으로써 0과 1 제거 2~150R까지 0~148 +2
 	fmt.Println("임의로 추출된 수 :", number)
 
-	for i := 1 ; i<=number;i++{
+	for i := 2 ; i<number;i++{ // 1과 number가 돌지 않음 2회 줄었음. /2 해서 절반만 돌려도 되는데 올림인지 내림인지 기억 안나네 이따 해주시겠지?
 		if number % i == 0{
 			count++
 		}
 	}
-	if count == 2 {
+	if count == 0 {
 		fmt.Println(number,"는(은) 소수입니다!")
 	}else{
 		fmt.Println(number,"는(은) 소수가 아닙니다!")
