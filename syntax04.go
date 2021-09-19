065a4b2c3e5d0046f93091e0d010b9192b5109f2
sunmorKhan <tjdah109@naver.com>
2021-09-19 20:14:39 +0900

    syntax04.go

--- syntax04.go
+++ syntax04.go
@@ -1,4 +1,4 @@
-package main //ver0.3
+package main //ver0.4
 
 import (
 	"fmt"
@@ -12,15 +12,17 @@ func main()  {
 
 	isPrime := true
 	number := rand.Intn(149) + 2
+	//number = 21
 	fmt.Println("임의로 추출된 수 :", number)
 
 	for i := 2 ; i<number;i++{
 		if number % i == 0{
 			isPrime = false
-			//count++ // count = count + 1 이라는 작업을 줄이겠다.
+			break // 소수가 아니라는 것을 알게된 순간 break가 걸림. 소수가 아닌 경우 반복수를 줄인다.
 		}
+		//fmt.Print(i," ")
 	}
-	if isPrime == true {
+	if isPrime{ // isPrime 자체가 bool타입으로 treu or false값만 가질 수 있음. 비교연산 제거.
 		fmt.Println(number,"는(은) 소수입니다!")
 	}else{
 		fmt.Println(number,"는(은) 소수가 아닙니다!")
